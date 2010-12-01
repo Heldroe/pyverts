5e82b5d85bc1043da81d1fea901bb649a9401380
Heldroe <heldroe@gmail.com>
2010-12-01 23:17:25 +0100

    added new bad shit

--- users/views.py
+++ users/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render_to_response
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.template import RequestContext
 
 def index(request):
   return HttpResponse("Index file !")
 
 def signup(request):
+  try:
+    name = request.POST['name']
+  except KeyError:
+    return render_to_response('users/signup.html', {
+        'err_msg': "You must set a name"},
+        context_instance=RequestContext(request)
+        )
+  try:
+    password = request.POST['password']
+    password2 = request.POST['password2']
+  except KeyError:
+    return render_to_response('users/signup.html', {
+        'err_msg': "You must set a password"},
+        context_instance=RequestContext(request)
+        )
+  else:
+    if password != password2:
+      return render_to_response('users/signup.html', {
+          'err_msg': "Passwords must be the same"},
+          context_instance=RequestContext(request)
+          )
+    else:
+      return render_to_response('users/signup_success.html')
   return render_to_response('users/signup.html')
+
