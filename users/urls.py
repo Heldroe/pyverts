0e31b9289137a8cb83b25b6db2971cc41e0ffcd0
MKRTCHIAN <mkrtchia@polytech.unice.fr>
2010-12-02 23:31:54 +0100

    url

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('users.views',
     (r'^login/$', 'login'),
     (r'^signup/success/$', 'signup_success'),
     (r'^login/success/$', 'login_success'),
-
+    (r'^(?P<profile_id>\d+)/$', 'view_profile'),
 )
