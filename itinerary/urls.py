de8e9da437922b085d1f4687d22bcc8881ec1512
jeando <favreau@polytech.unice.fr>
2010-12-02 23:03:22 +0100

    ...

---
+++ itinerary/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('itinerary.views',
+    (r'^$', 'trajet_index'),
+#    (r'^signup/$', 'signup'),
+#   (r'^login/$', 'login'),
+#   (r'^signup/success/$', 'signup_success'),
+
+)
