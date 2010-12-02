b829870f26cd8f6dfca0c74c9e9a6b2b1e83d60a
Heldroe <heldroe@gmail.com>
2010-12-03 00:20:00 +0100

    new profile model

--- users/models.py
+++ users/models.py
@@ -7,8 +7,8 @@ from cars.models import Car
 class Profile(models.Model):
     user = models.ForeignKey(User, unique=True)
 
-    first_name = models.CharField(min_length=2, max_length=50, default='')
-    last_name = models.CharField(min_length=2, max_length=100, default='')
+    first_name = models.CharField(max_length=50, default='')
+    last_name = models.CharField(max_length=100, default='')
     phone = models.CharField(max_length=30, default='')
     place = models.CharField(max_length=300, default='')
 
