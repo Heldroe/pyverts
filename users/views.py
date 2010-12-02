40ebce35bb26a8d8290c41fc2096ecd2fcb74d4f
Heldroe <heldroe@gmail.com>
2010-12-02 08:39:26 +0100

    updated users signup view

--- users/views.py
+++ users/views.py
@@ -7,28 +7,14 @@ def index(request):
   return HttpResponse("Index file !")
 
 def signup(request):
-  try:
-    name = request.POST['name']
-  except KeyError:
-    return render_to_response('users/signup.html', {
-        'err_msg': "You must set a name"},
-        context_instance=RequestContext(request)
-        )
-  try:
-    password = request.POST['password']
-    password2 = request.POST['password2']
-  except KeyError:
-    return render_to_response('users/signup.html', {
-        'err_msg': "You must set a password"},
-        context_instance=RequestContext(request)
-        )
-  else:
-    if password != password2:
-      return render_to_response('users/signup.html', {
-          'err_msg': "Passwords must be the same"},
-          context_instance=RequestContext(request)
-          )
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('/signup/success/')
     else:
-      return render_to_response('users/signup_success.html')
-  return render_to_response('users/signup.html')
+        form = SignupForm()
+
+    return render_to_response('users/signup.html', {
+        'form': form,
+    })
 
