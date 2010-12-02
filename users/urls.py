0265f9b7c3582bf960154f642b33b2cb8d8daffd
MKRTCHIAN <mkrtchia@polytech.unice.fr>
2010-12-02 11:41:02 +0100

    qqs modifs

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,6 @@ urlpatterns = patterns('users.views',
     (r'^$', 'index'),
     (r'^signup/$', 'signup'),
     (r'^signup/success/$', 'signup_success'),
+    (r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+
 )
--- users/views.py
+++ users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
+from django.contrib.auth.models import User
+
 
 from forms import SignupForm
 
