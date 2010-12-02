b980bc15c7dbddb38bd22cc2556f8474d6c75ca3
jeando <favreau@polytech.unice.fr>
2010-12-02 21:53:23 +0100

    itineraire

--- settings.py
+++ settings.py
@@ -97,6 +97,7 @@ INSTALLED_APPS = (
     'users',
     'cars',
     'evaluation',
+    'itinerary',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
