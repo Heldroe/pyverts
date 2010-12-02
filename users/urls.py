5425bf7eb2c1e07f65123b0cc891ea82a1065092
MKRTCHIAN <mkrtchia@polytech.unice.fr>
2010-12-02 20:37:44 +0100

    login

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('users.views',
     (r'^$', 'index'),
     (r'^signup/$', 'signup'),
+    (r'^login/$', 'login'),
     (r'^signup/success/$', 'signup_success'),
 
 )
