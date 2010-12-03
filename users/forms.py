c92e21b481c48e8826f174271f673e683fc4c8c6
Heldroe <heldroe@gmail.com>
2010-12-03 07:53:22 +0100

    FUUUUUUUUUUCK

--- users/forms.py
+++ users/forms.py
@@ -11,15 +11,15 @@ from cars.models import Car
 from itinerary.models import Itinerary
 
 class SignupForm(forms.Form):
-    name = forms.CharField(min_length=2, max_length=50, required=True,
+    name = forms.CharField(min_length=2, max_length=50, required=True, label='Pseudo',
         error_messages={'required': 'Name is missing',
                         'min_length': 'Minimal length : 2 chars',
                         'max_length': 'Maximal length : 50 chars'})
-    password = forms.CharField(min_length=4, required=True,
+    password = forms.CharField(min_length=4, required=True,label='Mot de passe',
                                widget=forms.PasswordInput,
         error_messages={'required': 'Password needed',
                        'min_length': '4 chars or more please'})
-    password2 = forms.CharField(min_length=4, required=True,
+    password2 = forms.CharField(min_length=4, required=True,label='Répétez le mot de passe',
                                 widget=forms.PasswordInput,
         error_messages={'required': 'Password x2 needed',
                        'min_length': '4 chars or more please'})
@@ -42,11 +42,11 @@ class SignupForm(forms.Form):
         return cleaned_data
 
 class LoginForm(forms.Form):
-    name = forms.CharField(min_length=2, max_length=50, required=True,
+    name = forms.CharField(min_length=2, max_length=50, required=True,label='Pseudo',
         error_messages={'required': 'Name is missing',
                         'min_length': 'Minimal length : 2 chars',
                         'max_length': 'Maximal length : 50 chars'})
-    password = forms.CharField(min_length=4, required=True,
+    password = forms.CharField(min_length=4, required=True,label='Mot de passe',
                                widget=forms.PasswordInput,
         error_messages={'required': 'Password needed',
                        'min_length': '4 chars or more please'})
@@ -65,15 +65,15 @@ class LoginForm(forms.Form):
         return cleaned_data
 
 class ProfileForm(ModelForm):
-    first_name = forms.CharField(min_length=2, max_length=50, required=False, initial="prenom",
+    first_name = forms.CharField(min_length=2, max_length=50, required=False, label='Prénom',
         error_messages={'min_length': 'Minimal length : 2 chars',
                         'max_length': 'Maximal length : 50 chars',})
-    last_name = forms.CharField(min_length=2, max_length=100, required=False,
+    last_name = forms.CharField(min_length=2, max_length=100, required=False, label='Nom de famille',
         error_messages={'min_length': 'Minimal length : 2 chars',
                         'max_length': 'Maximal length : 100 chars'})
-    phone = forms.RegexField(regex='^0[1-9]([-. ]?[0-9]{2}){4}$', required=False,
+    phone = forms.RegexField(regex='^0[1-9]([-. ]?[0-9]{2}){4}$', required=False,label='Numéro de téléphone',
         error_messages={'invalid': 'Format incorrect'})
-    place = forms.CharField(max_length=300, required=False,
+    place = forms.CharField(max_length=300, required=False, label='Votre emplacement',
         error_messages={'max_length': 'Taille maximale : 300 caractères'})
     class Meta:
         model = Profile
@@ -104,15 +104,15 @@ class NewCarForm(ModelForm):
         fields = ('model', 'places', 'consumption', 'essence_type', 'accessibility')
 
 class NewItineraryForm(ModelForm):
-    depart = forms.CharField(required=True, min_length=2, max_length=200,
+    depart = forms.CharField(required=True, min_length=2, max_length=200,label='Lieu de départ',
         error_messages={'required': 'Vous devez définir un lieu de départ',
                         'min_length': 'Le lieu doit faire au moins 2 caractères',
                         'max_length': 'Le lieu doit faire au maximum 200 caractères'})
-    arrival = forms.CharField(required=True, min_length=2, max_length=200,
+    arrival = forms.CharField(required=True, min_length=2, max_length=200,label='Lieu d\'arrivée',
         error_messages={'required': 'Vous devez définir un lieu d\'arrivée',
                         'min_length': 'Le lieu doit faire au moins 2 caractères',
                         'max_length': 'Le lieu doit faire au maximum 200 caractères'})
-    depart_hour = forms.CharField(required=True, min_length=10, max_length=100,
+    depart_hour = forms.CharField(required=True, min_length=10, max_length=100,label='Heure de départ',
         error_messages={'required': 'Vous devez définir une date de départ',
                         'min_length': 'La date doit faire au moins 10 caractères',
                         'max_length': 'La date ne doit pas dépasser 100 caractères'})
