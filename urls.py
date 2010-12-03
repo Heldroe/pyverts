c2f4ae4114dc98ecc152db7596cd44858327e140
Heldroe <heldroe@gmail.com>
2010-12-03 03:52:25 +0100

    news urls for static files

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -16,3 +17,8 @@ urlpatterns = patterns('django.views.generic.simple',
     # Uncomment the next line to enable the admin:
     # (r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}))
+
